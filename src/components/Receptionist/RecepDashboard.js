404b4efdac7d7463292fd6e6c2a3413d1ad6274a
Sachini Chathurangi <chathurangi666@gmail.com>
2019-04-10 17:10:20 +0530

    ui

--- src/components/Receptionist/RecepDashboard.js
+++ src/components/Receptionist/RecepDashboard.js
@@ -1,63 +1,178 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import AddPatient from '../Receptionist/AddPatient'
-import ViewPatient from '../common/ViewPatients'
+
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import Footer from '../layout/Footer'
-import { logoutUser } from "../../actions/authActions";
-import ViewAppoint from './ViewAppoint';
+import Footer from '../layout/Footer';
+import AddPatient from './AddPatient'
+import * as ReactBootstrap from 'react-bootstrap';
+import ViewPatient from '../common/ViewPatients';
+import Appointment from './ViewAppoint'
+import { logoutUser } from "../../actions/authActions"
 
-class Dashboard extends Component {
+
+class RecepDashboard extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {  
+      patientView: false,
+      patientadd: false,
+      appointment: false,
+    };
+  }
+
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
   };
+
+  updateView = () => {
+    this.setState({
+      patientView: true,
+      patientadd: false,
+      appointment: false,
+
+    })
+  }
+  updatePatientAdd = () => {
+    this.setState({
+        patientView: false,
+        patientadd: true,
+        appointment: false,
+    })
+  }
+  updateAppoint = () => {
+    this.setState({
+        patientView: false,
+        patientadd: false,
+        appointment: true,
+    })
+  }
+  
   render() {
-   
+
+    const buttonstyle = {
+      borderRadius: "10px", backgroundColor: "black", color: "white"
+    };
+
     return (
-      <Router>
-        <div className="container-fluid">
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link to={'/'} className="navbar-brand">Nawodaya</Link>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                  <Link to={'/'} className="nav-link">Home</Link>
-                </li>
+      <div className="App">
+
+        {/* header */}
+
+        <header className="main-header">
+          <a href="" className="logo">
+            <span className="logo-lg"><b>Admin</b>Nawodaya</span>
+          </a>
+
+          <nav className="navbar navbar-static-top">
+
+
+            <a href="" className="sidebar-toggle" data-toggle="push-menu" role="button">
+              <span className="sr-only">Toggle navigation</span>
+            </a>
+            <div className="navbar-custom-menu">
+              <ul className="nav navbar-nav">
                 <li className="nav-item">
-                <Link to={'/create'} className="nav-link">Create</Link>
+                  <ReactBootstrap.Button onClick={this.onLogoutClick} className="navbar-brand" style={{ color: "white" }}>Logout</ReactBootstrap.Button>
                 </li>
-                <li className="nav-item">
-                  <Link to={'/view'} className="nav-link">View</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={'/addAppoint'} className="nav-link">Appointments</Link>
-                </li>
-                <li className="nav-item">
-                 <Link  onClick={this.onLogoutClick} className="nav-link" >Logout</Link>
-                </li>
-                <li className="nav-item dropdown">
-        
-      </li>
-                </ul>
+              </ul>
             </div>
-          </nav> <br/>
-          <h2>Welcome to Nawodaya Hospital</h2> <br/>
-          <Switch>
-              <Route exact path='/create' component={ AddPatient } />
-              <Route path='/addAppoint' component={ ViewAppoint } />
-              <Route path='/view' component={ ViewPatient } />
-          </Switch>
-          <Footer/>
-        </div>
-      </Router>
-      
+          </nav>
+        </header>
+
+        <aside className="main-sidebar" style={{ position: "absolute" }}>
+          <section className="sidebar" style={{ position: "absolute" }}>
+            <div className="user-panel">
+              <div className="pull-left image">
+                <img src="images/admin.jpg" className="img-circle" alt="User Image" />
+              </div>
+              <div className="pull-left info">
+                <p>Alexander Pierce</p>
+                <a href=""><i className="fa fa-circle text-success"></i> Online</a>
+              </div>
+            </div>
+            <form action="#" method="get" className="sidebar-form">
+              <div className="input-group">
+                <input type="text" name="q" className="form-control" placeholder="Search..." />
+                <span className="input-group-btn">
+                  <button type="submit" name="search" id="search-btn" className="btn btn-flat"><i className="fa fa-search"></i>
+                  </button>
+                </span>
+              </div>
+            </form>
+            <ul className="sidebar-menu">
+              <li className="header">MAIN NAVIGATION</li>
+
+              <li className="treeview">
+                <button onClick={this.updatePatientAdd}
+                  aria-controls="example-collapse-text">
+                  <i className="fa fa-files-o"></i>
+                  <span>New Patient</span>
+                  <span className="pull-right-container">
+                    <span className="label label-primary pull-right">4</span>
+                  </span>
+                </button>
+                
+              </li>
+
+              <li className="treeview">
+                <button onClick={this.updateView}
+                  aria-controls="example-collapse-text">
+                  <i className="fa fa-files-o"></i>
+                  <span>View Patient</span>
+                  <span className="pull-right-container">
+                    
+                  </span>
+                </button>
+                
+              </li>
+
+              <li className="treeview">
+                <button onClick={this.updateAppoint} aria-controls="example-collapse-text"
+                >
+                  <i className="fa fa-pie-chart"></i>
+                  <span>Appointments</span>
+                  <span className="pull-right-container">
+                    <i className="fa fa-angle-left pull-right"></i>
+                  </span>
+                </button>
+                
+              </li>
+              <li>
+                <ul className="treeview-menu">
+                  <li><a href=""><i className="fa fa-circle-o"></i> Simple tables</a></li>
+                  <li><a href=""><i className="fa fa-circle-o"></i> Data tables</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="pages/calendar.html">
+                  <i className="fa fa-calendar"></i> Laboratorian<span></span>
+                  <span className="pull-right-container">
+                    <small className="label pull-right bg-red">3</small>
+                    <small className="label pull-right bg-blue">17</small>
+                  </span>
+                </a>
+              </li>
+
+            </ul>
+          </section>
+
+
+        </aside>
+
+        {this.state.patientView && (<ViewPatient />)}
+        {this.state.patientadd && (<AddPatient />)}
+        {this.state.appointment && (<Appointment />)}
+        
+        <Footer />
+
+      </div>
+
     );
   }
 }
-Dashboard.propTypes = {
+RecepDashboard.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
 };
@@ -67,4 +182,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   { logoutUser }
-)(Dashboard);
+)(RecepDashboard);
