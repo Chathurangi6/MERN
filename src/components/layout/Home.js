120a10401b9c834f7a789777f02d5d3baed641f2
Sachini Chathurangi <chathurangi666@gmail.com>
2019-04-24 22:02:02 +0530

    delete doc

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -9,16 +9,16 @@ class Slide extends Component {
           <div class="row flex-center pt-5 mt-3">
             <div class="col-md-12 col-lg-6 text-center text-md-left margins">
               <div class="white-text">
-                <h1 class="h1-responsive font-weight-bold mt-md-5 mt-0 wow fadeInLeft animated" data-wow-delay="0.3s" className="landing">Nawodaya
+                <h1 class="h1-responsive font-weight-bold mt-md-5 mt-0 wow fadeInLeft animated" data-wow-delay="0.3s" className="landing">Nawodaya 
+                Hospital
                   </h1>
-                  <h1 class="h1-responsive font-weight-bold mt-md-5 mt-0 wow fadeInLeft animated" data-wow-delay="0.3s" className="landing">Hospital
-                  </h1>              
-                <p class="wow fadeInLeft mb-3 animated" data-wow-delay="0.3s" style={{visibility: 'visible', animationName: 'fadeInLeft', animationDelay: '0.3s'}}>Lorem ipsum dolor sit amet, consectetur
+                 
+                <p class="wow fadeInLeft mb-3 animated" data-wow-delay="0.3s" className='paragraph'>Lorem ipsum dolor sit amet, consectetur
                   adipisicing elit. Rem repellendus quasi fuga nesciunt
                   dolorum nulla magnam veniam sapiente, fugiat! Commodi sequi non animi ea dolor molestiae
                   iste.
                 </p>     
-                <a href="/login" class="btn btn-unique btn-rounded font-weight-bold ml-lg-0 wow fadeInLeft waves-effect waves-light animated" data-wow-delay="0.3s" style={{visibility: 'visible', animationName: 'fadeInLeft' ,animationDelay: "0.3s"}}>Login</a>
+                <a href="/login" class="btn btn-unique btn-rounded font-weight-bold ml-lg-0 wow fadeInLeft waves-effect waves-light animated" data-wow-delay="0.3s" style={{visibility: 'visible', animationName: 'fadeInLeft' ,animationDelay: "0.3s",backgroundColor:'purple'}}>Login</a>
               </div>
             </div>
             <div class="col-md-12 col-lg-6 wow fadeInRight d-flex justify-content-center animated" data-wow-delay="0.3s" style={{visibility: 'visible', animationName: 'fadeInRight', animationDelay: '0.3s'}}>
